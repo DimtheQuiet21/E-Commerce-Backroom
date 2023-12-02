c80cdfa7f85bdbc0ed0d20a45b4071dd57d371df
Fisherman <aflee@wisc.edu>
2023-12-02 14:48:25 -0600

    Update the router again

--- Develop/routes/api/product-tag-routes.js
+++ Develop/routes/api/product-tag-routes.js
@@ -2,20 +2,19 @@ const router = require('express').Router();
 const { ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
-    // find all categories
-    // be sure to include its associated Products
     try{
-      const userData = await ProductTag.findAll({
-      });
-      if (!userData) {
-        res.status(404).json({ message: 'No Product Tags' });
-        return;
-      }
-     res.status(200).json(userData)
-    } catch (err) {
-      res.status(500).json(err)
-  
-    };
+        const userData = await ProductTag.findAll({
+          include: [{ model: Category},{model: Tag}],
+        });
+        if (!userData) {
+          res.status(404).json({ message: 'No user with this id!' });
+          return;
+        }
+       res.status(200).json(userData)
+      } catch (err) {
+        res.status(500).json(err)
+    
+      };
   });
 
   module.exports = router;
