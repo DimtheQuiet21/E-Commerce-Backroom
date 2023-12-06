4052f8958cd8adc8e6efed11e7a3ac35d64690d3
Fisherman <aflee@wisc.edu>
2023-12-06 16:50:15 -0600

    Fixed a delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -108,6 +108,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     };
+    const destroyData = await Category.destroy({where: {id:req.params.id}})
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
